f6959b58e074e936062f79eeb37626eff295cb6f
Supratim Mandal <supratimm98@gmail.com>
2026-03-18 23:32:04 +0530

    second commit

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <>
       <Navbar/>
+      <div className="text-5xl flex justify-center items-center h-screen">
+        Kaj cholche bhai 
+      </div>
     </>
   );
 }
